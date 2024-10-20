1d77cdeeed7f02a09640f66d553c1e33416b2582
Deepak Patidar <patidardeepak350@gmail.com>
2024-10-20 13:06:45 +0530

    update color

--- src/component/Header.js
+++ src/component/Header.js
@@ -26,10 +26,10 @@ function Header({ isAuthenticated, setIsAuthenticated }) {
   };
 
   return (
-    <header className="fixed top-0 left-0 w-full flex flex-row items-center justify-between sm:justify-around p-4 border-b-2 bg-gradient-to-r from-cyan-500 to-gray-500  z-50 shadow-md">
+    <header className="fixed top-0 left-0 w-full flex flex-row items-center justify-between sm:justify-around p-4 border-b-2 bg-gradient-to-r from-cyan-500 to-red-300  z-50 shadow-md">
       <Link
         to="/home" // Use Link for internal navigation
-        className="flex items-center h-10 px-4 bg-gradient-to-r from-gray-900 via-gray-600 to-red-300 rounded-tl-full rounded-br-full font-bold uppercase italic text-white hover:opacity-90"
+        className="flex items-center h-10 px-4 bg-gradient-to-r from-gray-900 via-gray-600 to-gray-500 rounded-tl-full rounded-br-full font-bold uppercase italic text-white hover:opacity-90"
       >
        Diary
       </Link>
